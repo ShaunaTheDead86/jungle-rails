d23af2b001bea397cba1b7045f09b162070fb930
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-14 16:13:30 -0400

    rearranging user_spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe User, type: :model do
       expect(@user).to have_attributes(:email => 'shaunathedead86@gmail.com')
     end
 
+    it 'should validate a password' do
+      @user = User.new(:password => 'password')
+
+      expect(@user).to have_attributes(:password => 'password')
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
     it 'should validate emails with spaces before or after' do
       @user = User.new(:email => '  shaunathedead86@gmail.com  ')
       @user[:email].strip!
@@ -15,22 +23,10 @@ RSpec.describe User, type: :model do
       expect(@user).to have_attributes(:email => 'shaunathedead86@gmail.com')
     end
 
-    it 'should validate a password' do
-      @user = User.new(:password => 'password')
-
-      expect(@user).to have_attributes(:password => 'password')
-    end
-
     it 'should not validate when email and password don\'t match' do
       @user = User.new(:email => 'shaunathedead86@gmail.com', :password => 'password')
 
       expect(@user).to_not have_attributes(:email => nil, :password => nil)
     end
-
-    
-  end
-
-  describe '.authenticate_with_credentials' do
-    # examples for this class method here
   end
 end
